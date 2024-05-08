acc5fcc131a22d48f4005471214ff43333119a3b
Mouaidnasab <65204173+Mouaidnasab@users.noreply.github.com>
2024-05-09 00:01:05 +0300

    change

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+var swiper = new Swiper(".swiper", {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 15,
+    speed: 300,
+    loop: false,
+    rotate: true,
+    mousewheel: {
+    invert: false,
+  },
+});
