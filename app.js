ccba86d310735cff86815836bc6df9c09fa509c6
Mouaidnasab <65204173+Mouaidnasab@users.noreply.github.com>
2024-05-14 15:51:30 +0300

    add mayor page

--- app.js
+++ app.js
@@ -20,3 +20,8 @@ var swiper = new Swiper(".swiper", {
 
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+function auto_grow(element) {
+  element.style.height = "5px";
+  element.style.height = (element.scrollHeight) + "px";
+}
